87568cc8a3409e0dde2c23b10a70a1a220ab2336
Siim Noomen <siimnoomen@gmail.com@>
2023-06-22 12:03:08 +0300

    loading for login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -14,9 +14,10 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { login, logOut } from "../actions/userActions";
+import Loading from "../components/Loading";
 
 export default function SignIn() {
-  const { userInfo } = useSelector((state) => state.userLogin);
+  const { userInfo, loading } = useSelector((state) => state.userLogin);
   const { isAuthenticated } = userInfo;
 
   const dispatch = useDispatch();
@@ -43,96 +44,100 @@ export default function SignIn() {
     }
   };
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <Card sx={{ p: 2, mt: 2 }}>
-        <Box
-          sx={{
-            marginTop: 2,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <Container component="main" maxWidth="xs">
+        <Card sx={{ p: 2, mt: 2 }}>
           <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              marginTop: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <TextField
-              color="secondary"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              color="secondary"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="secondary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                ":hover": {
-                  bgcolor: "secondary.main", // theme.palette.primary.main
-                  color: "primary.main",
-                },
-              }}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
             >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item sm>
-                <Link
-                  component={RouterLink}
-                  color="text.secondary"
-                  to="/forgot-login"
-                  variant="body2"
-                >
-                  Forgot password?
-                </Link>
+              <TextField
+                color="secondary"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                color="secondary"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="secondary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  ":hover": {
+                    bgcolor: "secondary.main", // theme.palette.primary.main
+                    color: "primary.main",
+                  },
+                }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item sm>
+                  <Link
+                    component={RouterLink}
+                    color="text.secondary"
+                    to="/forgot-login"
+                    variant="body2"
+                  >
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item sm>
+                  <Link
+                    color="text.secondary"
+                    component={RouterLink}
+                    to="/signup"
+                    variant="body2"
+                  >
+                    {"Don't have an account? Sign up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item sm>
-                <Link
-                  color="text.secondary"
-                  component={RouterLink}
-                  to="/signup"
-                  variant="body2"
-                >
-                  {"Don't have an account? Sign up"}
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Card>
-    </Container>
-  );
+        </Card>
+      </Container>
+    );
+  }
 }
